e98874e09a4f48e0367d75efca98f841e9f3e1e2
Broderick Carlin <broderick.carlin@gmail.com>
2021-03-03 01:16:36 -0600

    Initial commit of things

---
+++ main.py
@@ -0,0 +1,64 @@
+import requests
+import os
+import json
+
+# Set this envvar to your Blaseball cookie
+token_key = "BLASEBALL_TOKEN"
+
+def get_user(cookie):
+    r = requests.get('https://www.blaseball.com/api/getUser', cookies={"connect.sid": cookie})
+    return r.json()
+
+def get_active_bets(cookie):
+    r = requests.get('https://www.blaseball.com/api/getActiveBets', cookies={"connect.sid": cookie})
+    return r.json()
+
+def get_games_to_bet_on(cookie):
+    r = requests.get('https://www.blaseball.com/events/streamData', cookies={"connect.sid": cookie}, stream=True)
+    if r.encoding is None:
+        r.encoding = 'utf-8'
+
+    for line in r.iter_lines(decode_unicode=True):
+        if line:
+            games = json.loads(line[6:])['value']['games']['tomorrowSchedule']
+            return games
+            
+def bet_on_game(cookie, user_id, team_id, game_id, amount):
+    body = json.dumps({u"amount": amount, u"userId": user_id, u"targets": [team_id, game_id], u"type": 0})
+    headers = {"Host": "www.blaseball.com", "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:88.0) Gecko/20100101 Firefox/88.0", "Accept": "*/*", "Accept-Language": "en-US,en;q=0.5", "Accept-Encoding": "gzip, deflate, br", "Content-Type": "application/json", "Origin": "https://www.blaseball.com", "Content-Length": "162", "Connection": "keep-alive", "Sec-Fetch-Dest": "empty", "Sec-Fetch-Mode": "cors", "Sec-Fetch-Site": "same-origin", "Sec-GPC": "1"}
+    print(body)
+    #401b3e6f-6ae9-4032-b0f9-2ec0220b1155 -- (57ec08cc-0411-4643-b304-0e80dbc15ac7)Mexico City Wild Wings vs (a37f9158-7f82-46bc-908c-c9e2dda7c33b)Breckenridge Jazz Hands
+    # Target: {"amount":1140,"userId":"a783e0f5-7933-49a4-827f-f432b3967cdc","targets":["b63be8c2-576a-4d6e-8daf-814f8bcea96f","d1a4ab89-dd65-444a-b3f5-f7722c5959df"],"type":0}
+    # actual: {"amount":1140,"userId":"a783e0f5-7933-49a4-827f-f432b3967cdc","targets":["a37f9158-7f82-46bc-908c-c9e2dda7c33b","401b3e6f-6ae9-4032-b0f9-2ec0220b1155"],"type":0}
+    r = requests.post('https://www.blaseball.com/api/bet', cookies={"connect.sid": cookie}, data=body, headers=headers)
+    print(r.text)
+    return r.status_code == 200
+
+
+if __name__ == "__main__":
+    cookie = os.getenv(token_key)
+    user = get_user(cookie)
+    bets = get_active_bets(cookie)
+    games = get_games_to_bet_on(cookie)
+
+    max_bet = 1000
+
+    print("Have {} coins to bet".format(user["coins"]))
+
+    for game in games:
+        # print(game.keys())
+        print("{} -- ({}){} vs ({}){}".format(game['id'],  game['homeTeam'], game['homeTeamName'],  game['awayTeam'], game['awayTeamName']))
+        print("{}: {:.2f}% vs {}: {:.2f}%".format(game['homeTeamName'], game['homeOdds'] * 100, game['awayTeamName'], game['awayOdds'] * 100))
+        user = get_user(cookie)
+        bet = user["coins"] if user["coins"] < max_bet else max_bet
+
+        if bet == 0:
+            continue
+        
+        if game['homeOdds'] > game['awayOdds']:
+            print("Betting on: {}".format(game['homeTeamName']))
+            bet_on_game(cookie, user["id"], game['homeTeam'], game['id'], bet)
+        else:
+            print("Betting on: {}".format(game['awayTeamName']))
+            bet_on_game(cookie, user["id"], game['awayTeam'], game['id'], bet)
+        print("\n")
